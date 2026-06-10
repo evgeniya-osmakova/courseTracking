e32adaec483363a60e25b35b5f4ac5685fab72da
Evgeniya Osmakova <evgeniya.osmakova@gmail.com>
2026-06-10 07:00:32 +0200

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import React from 'react';
 import './globals.css';
@@ -5,6 +6,13 @@ import './globals.css';
 import { Provider } from './components/Provider/Provider';
 
 const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+    icons: {
+        icon: '/icon.svg',
+        shortcut: '/icon.svg',
+    },
+};
 
 export default function RootLayout({
     children,
